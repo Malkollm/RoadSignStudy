32f7418a8e7824d1b4a41687fcc3f7180dd8352d
Marsel <fatkullinmars23@gmail.com>
2021-01-07 22:43:55 +0500

    Init commit

---
+++ src/main/java/ru/malkollm/roadsign/RoadSignApplication.java
@@ -0,0 +1,13 @@
+package ru.malkollm.roadsign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadSignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadSignApplication.class, args);
+	}
+
+}
